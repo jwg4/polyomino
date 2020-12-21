e5a69cd5be12414632352fe9c0418199d9c71cc1
Jack Grahl <jack.grahl@gmail.com>
2020-12-21 13:20:05 +0000

    Tiling problem class.

--- chessboard.py
+++ chessboard.py
@@ -1,16 +1,14 @@
 from exact_cover_np import get_exact_cover
 
-from polyomino import tiling_to_array
+from polyomino import TilingProblem
 from polyomino.board import Rectangle
-from polyomino.constant import PENTOMINOS
+from polyomino.constant import PENTOMINOS, TETROMINOS
 
 
 if __name__ == '__main__':
     board = Rectangle(8, 8)
-    SQUARE = [(0, 0), (1, 0), (1, 1), (0, 1)]
-    TILES = list(PENTOMINOS.values()) + [SQUARE]
-    key, array = tiling_to_array(TILES, board)
-    solution = get_exact_cover(array)
-    tiles = [key[s] for s in solution]
-    print(board.format_tiling(tiles))
+    TILES = list(PENTOMINOS.values()) + [TETROMINOS['Square']]
+    problem = TilingProblem(board, TILES)
+    problem.solve()
+    print(problem.format_output())
 
--- polyomino/__init__.py
+++ polyomino/__init__.py
@@ -1,5 +1,7 @@
 import numpy as np
 
+from exact_cover_np import get_exact_cover
+
 
 def selector_vector(n, i):
     return [i == j for j in range(0, n)]
@@ -22,3 +24,17 @@ def tiling_to_array(tiles, shape):
     key, data = tiling_to_problem(tiles, shape)
     array = np.array(data, dtype=np.int32)
     return key, array
+
+
+class TilingProblem(object):
+    def __init__(self, board, tiles):
+        self.board = board
+        self.tiles = tiles
+
+    def solve(self):
+        key, array = tiling_to_array(self.tiles, self.board)
+        solution = get_exact_cover(array)
+        self.solution = [key[s] for s in solution]
+
+    def format_output(self):
+        return self.board.format_tiling(self.solution)
