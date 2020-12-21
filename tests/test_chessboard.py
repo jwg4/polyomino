7e1b974f625b8db1f37b7eae0c3716a72eb45d4c
Jack Grahl <jack.grahl@gmail.com>
2020-12-21 13:33:31 +0000

    Make the example code into a test.

--- tests/test_chessboard.py
+++ tests/test_chessboard.py
@@ -1,14 +1,13 @@
-from exact_cover_np import get_exact_cover
-
 from polyomino import TilingProblem
 from polyomino.board import Rectangle
 from polyomino.constant import PENTOMINOS, TETROMINOS
 
 
-if __name__ == '__main__':
+def test_tile_chessboard():
     board = Rectangle(8, 8)
     TILES = list(PENTOMINOS.values()) + [TETROMINOS['Square']]
     problem = TilingProblem(board, TILES)
     problem.solve()
-    print(problem.format_output())
+    actual = problem.solution
+    assert len(actual) == 13
 
