9d345c45097230e901dbce4d5e0c96e879cffde3
Jack Grahl <jack.grahl@gmail.com>
2021-02-09 23:16:56 +0000

    Implement python output.

--- polyomino/solution.py
+++ polyomino/solution.py
@@ -5,3 +5,9 @@ class Solution(object):
 
     def display(self):
         return self.board.format_tiling(self.tiling)
+
+    def python(self):
+        return "\n".join(self._gen_python())
+
+    def _gen_python(self):
+        yield "TILING = %s" % (repr(self.tiling), )
