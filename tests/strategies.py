61472194e7448b640cf72e91f66d527e9b2b457d
Jack Grahl <jack.grahl@gmail.com>
2021-03-29 22:42:17 +0100

    Add more strategies.

--- tests/strategies.py
+++ tests/strategies.py
@@ -21,7 +21,19 @@ def make_box(x, y):
 
 boxes = builds(make_box, integers(1, 10), integers(1, 10))
 
-simple_polyominos = one_of(h_lines, v_lines, boxes)
+
+def make_v_shape(x, y):
+    return [(0, i) for i in range(0, x)] + [(j, 0) for j in range(0, y)]
+
+
+def make_j_shape(x, y):
+    return [(0, i) for i in range(0, x + y)] + [(1, i) for i in range(x)]
+
+
+v_shapes = builds(make_v_shape, integers(2, 10), integers(2, 10))
+j_shapes = builds(make_j_shape, integers(2, 5), integers(2, 5))
+
+simple_polyominos = one_of(h_lines, v_lines, boxes, v_shapes, j_shapes)
 
 small_polyominos = sampled_from(ALL_UP_TO_PENTOMINOS)
 
