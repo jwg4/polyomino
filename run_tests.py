c6a79b77bf838275c7e101e315d71604af1ecf3b
Jack Grahl <jack.grahl@gmail.com>
2021-07-07 23:13:58 +0100

    Whitespace.

--- run_tests.py
+++ run_tests.py
@@ -12,5 +12,5 @@ def run_doctests():
     for file in os.listdir("examples/"):
         if not file.endswith(".md"):
             continue
-        filepath = os.path.join("examples/", file) 
+        filepath = os.path.join("examples/", file)
         doctest.testfile(filepath, optionflags=doctest.ELLIPSIS, raise_on_error=True)
